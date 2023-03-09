6058a7609aed9e9381c17f85c90647d5767ceef0
UrOkaSan <tashtanaliev11@mail.ru>
2023-03-09 20:19:36 +0600

    Initial Commit

---
+++ lesson2M5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson2M5
+//
+//  Created by User on 9/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
